0fe6ee312d59b17655e6dcc9a3ac41551793b29f
bhargav08-code <bhargavgarge1828@gmail.com>
2024-05-07 13:40:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+const App = () => {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const [isLoadingLogin, setIsLoadingLogin] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoadingLogin(true);
+    setTimeout(() => {
+      setIsLoadingLogin(false);
+      loginWithRedirect();
+    }, 5000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          {isAuthenticated ? (
+            <>
+              <h3 className="text-4xl font-bold mb-5">Hello {user.name}!</h3>
+              <button
+                onClick={(e) => logout()}
+                className="btn btn-outline btn-error"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <h1 className="text-5xl font-bold">Hello There !</h1>
+              <p className="py-6">
+                The Auth0 Login Package is a versatile authentication solution
+                designed to streamline the login process for applications while
+                offering users multiple authentication options. Built on top of
+                Auth0's powerful authentication platform, this package provides
+                a seamless and secure authentication experience for both
+                developers and end-users.
+              </p>
+              <button
+                onClick={handleLoginClick}
+                className="btn btn-outline btn-accent"
+                disabled={isLoadingLogin} // Disable the button while loading
+              >
+                {isLoadingLogin ? (
+                  <span className="loading loading-infinity loading-xl text-accent"></span>
+                ) : (
+                  "Login"
+                )}
+              </button>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
